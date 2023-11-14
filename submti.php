49e9c35f0f431bca627af918d0264f685038a02b
pan150350pan@gmail.com <pan150350pan@gmail.com>
2023-11-14 11:08:34 +0700

    week5

---
+++ submti.php
@@ -0,0 +1,20 @@
+<?php
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $city = $_POST['city'];
+    $web = $_POST['web'];
+    $role = $_POST['role'];
+    $sign = $_POST['sign'];
+    
+
+    echo '$email: '.$email.'<br>';
+    echo '$passward: '.$passward.'<br>';
+    echo '$city: '.$city.'<br>';
+    echo '$ewb: '.$web.'<br>';
+    echo '$role: '.$role.'<br>';
+    echo '$sign: '.$sign.'<br>';
+
+
+
+
+?>
