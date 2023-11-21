203e4c48c05718739eb7032d73eba1f24e310302
pan150350pan@gmail.com <pan150350pan@gmail.com>
2023-11-21 11:06:27 +0700

    add submti.php

--- submti.php
+++ submti.php
@@ -4,8 +4,43 @@
     $city = $_POST['city'];
     $web = $_POST['web'];
     $role = $_POST['role'];
-    $sign = $_POST['sign'];
+    $sign1 = $_POST['sign1'];
+    $sign2 = $_POST['sign2'];
+    $sign3 = $_POST['sign3'];
+
+    $servernane = 'localhost:'
+    $usernane = 'root'
+    $password = "";
+    $dbname = "db_emoployee"
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("". $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO employee (username, password, city, webserver,role) 
+            VALUES ($email,$passwoed,$cilty,$web,$role)";
     
+    $reult = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+    } else{
+        echo"error".$sql."<br>"$conn->error;
+    }
+    $conn->close();
+    ?>
+
+
+
+
+
+
+
+
+
+
+
 
     echo '$email: '.$email.'<br>';
     echo '$passward: '.$passward.'<br>';
